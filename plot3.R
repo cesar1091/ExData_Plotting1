c8ca9440a2921c83c14df262c15e67ae56bcfcfe
cesar1091 <59399364+cesar1091@users.noreply.github.com>
2020-01-14 17:50:34 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,38 +1,21 @@
-UCI_url = 
-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
-if(!file.exists("household_power_consumption.txt")){
-  download.file(UCI_url, destfile = "exdata_data_household_power_consumption.zip")
-  unzip("exdata_data_household_power_consumption.zip")
+fileurl = "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists('./Electric power consumption.zip')){
+  download.file(fileurl,"./Electric power consumption.zip")
+  unzip("Electric power consumption.zip", exdir = getwd())
 }
 
-txtfile   = "./household_power_consumption.txt"
-data_begin = grep("1/2/2007", readLines(txtfile))[1] - 1
-data_end   = grep("3/2/2007", readLines(txtfile))[1] - 1
+data <- read.table("./household_power_consumption.txt", header=T, sep=";", na.strings = "?")
 
-UCI_data = read.table(file  = txtfile,
-                      sep   = ";",
-                      skip  = data_begin,
-                      nrows = data_end - data_begin,
-                      na.strings = "?",
-                      col.names  = strsplit(x = readLines(txtfile, n = 1),
-                                            split = ";")[[1]])
-UCI_data$Time <- as.POSIXct(paste(UCI_data$Date, UCI_data$Time),
-                            format="%d/%m/%Y %H:%M:%S")
-UCI_data$Date <- NULL
+data[,"Date"] <- as.Date(data[,"Date"],format = "%d/%m/%Y")
+edited_data<-subset(data,Date == "2007-02-01" | Date == "2007-02-02")
+edited_time <-strptime(paste(edited_data$Date,edited_data$Time,sep = " "), "%Y-%m-%d %H:%M:%S")
 
-dir.create(file.path(getwd(), "figure"), showWarnings = FALSE)
-cwd = getwd()
-setwd(file.path(getwd(), "figure"))
-png(file = "plot3.png", width = 480, height = 480)
-Sys.setlocale("LC_TIME", "en_US")
-plot(x = UCI_data$Time, type = "l",
-     y = UCI_data$Sub_metering_1,
-     ylab = "Energy sub metering",
-     xlab = "")
-lines(UCI_data$Time, UCI_data$Sub_metering_2, col = "red")
-lines(UCI_data$Time, UCI_data$Sub_metering_3, col = "blue")
-legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+#PLOT3
+
+plot(edited_time, edited_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(edited_time, edited_data$Sub_metering_2, type="l", col = "red")
+lines(edited_time, edited_data$Sub_metering_3, type="l", col = "blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1)
+
+dev.copy(png, file = "plot3.png", height=480, width=480)
 dev.off()
-setwd(cwd)
