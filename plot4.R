9b2b44573792fd3c4da9601b931e81af7d17abc6
cesar1091 <59399364+cesar1091@users.noreply.github.com>
2020-01-14 17:51:21 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,50 +1,29 @@
-UCI_url = 
-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
-if(!file.exists("household_power_consumption.txt")){
-  download.file(UCI_url, destfile = "exdata_data_household_power_consumption.zip")
-  unzip("exdata_data_household_power_consumption.zip")
+fileurl = "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists('./Electric power consumption.zip')){
+  download.file(fileurl,"./Electric power consumption.zip")
+  unzip("Electric power consumption.zip", exdir = getwd())
 }
 
-txtfile   = "./household_power_consumption.txt"
-data_begin = grep("1/2/2007", readLines(txtfile))[1] - 1
-data_end   = grep("3/2/2007", readLines(txtfile))[1] - 1
+data <- read.table("./household_power_consumption.txt", header=T, sep=";", na.strings = "?")
 
-UCI_data = read.table(file  = txtfile,
-                      sep   = ";",
-                      skip  = data_begin,
-                      nrows = data_end - data_begin,
-                      na.strings = "?",
-                      col.names  = strsplit(x = readLines(txtfile, n = 1),
-                                            split = ";")[[1]])
-UCI_data$Time <- as.POSIXct(paste(UCI_data$Date, UCI_data$Time),
-                            format="%d/%m/%Y %H:%M:%S")
-UCI_data$Date <- NULL
+data[,"Date"] <- as.Date(data[,"Date"],format = "%d/%m/%Y")
+edited_data<-subset(data,Date == "2007-02-01" | Date == "2007-02-02")
+edited_time <-strptime(paste(edited_data$Date,edited_data$Time,sep = " "), "%Y-%m-%d %H:%M:%S")
 
-dir.create(file.path(getwd(), "figure"), showWarnings = FALSE)
-cwd = getwd()
-setwd(file.path(getwd(), "figure"))
-png(file = "plot4.png", width = 480, height = 480)
-Sys.setlocale("LC_TIME", "en_US")
-par(mfcol = c(2,2))
-# Repeating plot 2
-with(UCI_data, plot(x = Time, type = "l",
-                    y = Global_active_power,
-                    ylab = "Global Active Power (kilowatts)",
-                    xlab = ""))
-# Repeating plot 3, but without legend border
-plot(x = UCI_data$Time, type = "l",
-     y = UCI_data$Sub_metering_1,
-     ylab = "Energy sub metering",
-     xlab = "")
-lines(UCI_data$Time, UCI_data$Sub_metering_2, col = "red")
-lines(UCI_data$Time, UCI_data$Sub_metering_3, col = "blue")
-legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       bty = "n")
-# New plots
-with(UCI_data, plot(x = Time, type = "l", y = Voltage, xlab = "datetime"))
-with(UCI_data, plot(x = Time, type = "l", y = Global_reactive_power,
-                    xlab = "datetime"))
+#PLOT4
+
+par(mfrow = c(2, 2))
+
+plot(edited_time, edited_data$Global_active_power, type="l", ylab="Global Active Power", xlab="")
+
+plot(edited_time, edited_data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(edited_time, edited_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(edited_time, edited_data$Sub_metering_2, type="l", col = "red")
+lines(edited_time, edited_data$Sub_metering_3, type="l", col = "blue")
+legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1,1), lwd = c(1, 1,1), bty="n")
+
+plot(edited_time, edited_data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.copy(png, file = "plot4.png", height=480, width=650)
 dev.off()
-setwd(cwd)
